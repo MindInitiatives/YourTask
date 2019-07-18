414dbc113ceb2fbdcd92e052ec761f8271de4eea
MindInitiatives <stevyn258@yahoo.com>
2019-07-18 16:24:52 +0100

    Updated the Login and Registration Activity

--- app/src/main/java/com/mindinitiatives/yourtask/MainActivity.java
+++ app/src/main/java/com/mindinitiatives/yourtask/MainActivity.java
@@ -1,13 +1,27 @@
 package com.mindinitiatives.yourtask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup=findViewById(R.id.signup_txt);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/mindinitiatives/yourtask/RegistrationActivity.java
+++ app/src/main/java/com/mindinitiatives/yourtask/RegistrationActivity.java
@@ -1,13 +1,93 @@
 package com.mindinitiatives.yourtask;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText email, pass;
+    private Button btnReg;
+    private TextView login_txt;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mDialog=new ProgressDialog(this);
+
+        email=findViewById(R.id.email_reg);
+        pass=findViewById(R.id.password_reg);
+
+        btnReg=findViewById(R.id.reg_btn);
+
+        login_txt=findViewById(R.id.login_txt);
+
+        login_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mEmail=email.getText().toString().trim();
+                String mPass=pass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mEmail))
+                {
+                    email.setError("Required Field...");
+                    return;
+                }
+                if (TextUtils.isEmpty(mPass))
+                {
+                    pass.setError("Required Field...");
+                    return;
+                }
+
+                mDialog.setMessage("Processing...");
+                mDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            mDialog.dismiss();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Problem !!! Could not Sign you in", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
