8d526a6e83eef49ea334463ae4266be76ce8042b
MindInitiatives <stevyn258@yahoo.com>
2019-07-18 17:39:04 +0100

    Created Model for data-binding

--- app/src/main/java/com/mindinitiatives/yourtask/MainActivity.java
+++ app/src/main/java/com/mindinitiatives/yourtask/MainActivity.java
@@ -1,14 +1,33 @@
 package com.mindinitiatives.yourtask;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView signup;
+    private EditText email;
+    private EditText pass;
+    private Button btnLogIn;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mDialog;
 
 
     @Override
@@ -16,7 +35,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth= FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() !=null)
+        {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
+
+        mDialog=new ProgressDialog(this);
+
         signup=findViewById(R.id.signup_txt);
+        email=findViewById(R.id.email_login);
+        pass=findViewById(R.id.password_login);
+        btnLogIn=findViewById(R.id.login_btn);
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail=email.getText().toString().trim();
+                String mPass=pass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mEmail))
+                {
+                    email.setError("Required Field...");
+                    return;
+                }
+                if (TextUtils.isEmpty(mPass))
+                {
+                    pass.setError("Required Field...");
+                    return;
+                }
+
+                mDialog.setMessage("Processing...");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Problem !!! Could not Sign you in", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+
+                    }
+                });
+            }
+        });
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
